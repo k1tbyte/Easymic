4de39c3bcba312163e1c347dd378cf9a8fa5b6e8
kitbyte <kit8byte@gmail.com>
2025-11-18 19:16:30 +0200

    feat: implement executable asset retrieval for update management and improve update handling logic

--- src/Resources/Resource.h
+++ src/Resources/Resource.h
@@ -28,6 +28,8 @@
 
 #define IDD_SETTINGS_GENERAL                        201
 #define IDC_SETTINGS_AUTOSTART                      202
+#define IDC_SETTINGS_UPDATES_ENABLED                203
+#define IDC_SETTINGS_CLEAR_SKIPPED                  204
 
 #define IDD_SETTINGS_INDICATOR                      251
 #define IDC_SETTINGS_INDICATOR_COMBO                252
